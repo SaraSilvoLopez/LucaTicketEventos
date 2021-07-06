edf07f2fc079f94997882eb73fbb33cf1823b62e
SaraSilvoLopez <you@example.com>
2021-07-06 16:54:40 +0200

    Estructura proyecto

---
+++ LucaTicketEventoService/src/main/java/com/example/demo/LucaTicketEventoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaTicketEventoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaTicketEventoServiceApplication.class, args);
+	}
+
+}
