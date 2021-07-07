3858d5e30d952078b0e13168d263cc82b21ed1ac
unknown <you@example.com>
2021-07-07 17:14:24 +0200

    actualizacion

--- LucaTicketEventoService/src/main/java/com/example/spring/controller/EventoController.java
+++ LucaTicketEventoService/src/main/java/com/example/spring/controller/EventoController.java
@@ -1,13 +1,20 @@
 package com.example.spring.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.spring.model.Evento;
 import com.example.spring.repository.EventoRepository;
@@ -31,16 +38,72 @@ public class EventoController {
 	@Autowired
 	private EventoRepository eventoRepository;
 
+	/**
+	 * Método para añadir evento
+	 * 
+	 * @param evento
+	 * @return Evento
+	 */
 	@PostMapping("/eventos/add")
 	public Evento addEvento(@RequestBody Evento evento) {
 		logger.info("---- Se ha invocado el microservicio EVENTOS/ADD");
 		return eventoRepository.save(evento);
 	}
 
+	/**
+	 * Método para listar todos los eventos de la BBDDs
+	 * 
+	 * @return List<Evento>
+	 */
 	@GetMapping("/eventos")
 	public List<Evento> getEventos() {
 		logger.info("---- Se ha invocado el microservicio EVENTOS/GETEVENTOS");
 		return eventoRepository.findAll();
 	}
 
+	/**
+	 * Método para eliminar evento por id. Si no lo encuentra o hay una excepcion
+	 * devuelte estado NOT FOUND
+	 * 
+	 * @param id
+	 * @return ResponseEntity<?>
+	 */
+	@DeleteMapping("/eventos/delete/{id}")
+	public ResponseEntity<?> deleteEvento(@PathVariable String id) {
+		logger.info("---- Se ha invocado el microservicio EVENTOS/DELETE/{id}");
+		try {
+			if (eventoRepository.findById(id).isPresent()) {
+				eventoRepository.deleteById(id);
+				return ResponseEntity.ok().build();
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+	/**
+	 * Método para modificar evento por id. Si no lo encuentra o hay una excepcion
+	 * devuelte estado NOT FOUND
+	 * 
+	 * @param evento
+	 * @return
+	 */
+	@PutMapping("/eventos/update")
+	public ResponseEntity<?> updateEvento(@RequestBody Evento evento) {
+		logger.info("---- Se ha invocado el microservicio EVENTOS/UPDATE");
+		try {
+			if (eventoRepository.findById(evento.getId()).isPresent()) {
+				Evento eventoModificado = eventoRepository.save(evento);
+				URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+						.buildAndExpand(eventoModificado.getId()).toUri();
+				return ResponseEntity.created(location).build();
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
 }
--- LucaTicketEventoService/src/main/java/com/example/spring/model/Recinto.java
+++ LucaTicketEventoService/src/main/java/com/example/spring/model/Recinto.java
@@ -20,14 +20,12 @@ import lombok.ToString;
  * 
  * @version 1.0
  */
-/*
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
-*/
 public class Recinto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -36,54 +34,5 @@ public class Recinto implements Serializable {
 	private String lugar;
 	private Integer aforo;
 	private String ciudad;
-	
-	public Recinto() {
-
-	}
-
-	public Recinto(String nombre, String lugar, Integer aforo, String ciudad) {
-		super();
-		this.nombre = nombre;
-		this.lugar = lugar;
-		this.aforo = aforo;
-		this.ciudad = ciudad;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getLugar() {
-		return lugar;
-	}
-
-	public void setLugar(String lugar) {
-		this.lugar = lugar;
-	}
-
-	public Integer getAforo() {
-		return aforo;
-	}
-
-	public void setAforo(Integer aforo) {
-		this.aforo = aforo;
-	}
-
-	public String getCiudad() {
-		return ciudad;
-	}
-
-	public void setCiudad(String ciudad) {
-		this.ciudad = ciudad;
-	}
-
-	@Override
-	public String toString() {
-		return "Recinto [nombre=" + nombre + ", lugar=" + lugar + ", aforo=" + aforo + ", ciudad=" + ciudad + "]";
-	}
 
 }
